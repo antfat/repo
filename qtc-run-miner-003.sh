198df2c89a6177e58f43ab32ba4e2a1cafb4e770
antfat <32416518+antfat@users.noreply.github.com>
2025-07-28 20:08:16 +0300

    Delete qtc-run-miner-003.sh

--- qtc-run-miner-003.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-        cuda_count=$(nvidia-smi | grep " RTX" | wc -l)
-            for (( i=0; i<cuda_count; i++ )); do
-                export CUDA_VISIBLE_DEVICES=$i
-                LD_LIBRARY_PATH=. && CUDA_VISIBLE_DEVICES=$i ./qubitcoin-miner-opt -a qhash -o ru.luckypool.io:8611 -u bc1qstxffvn68pkwexp4ly99p6kqeyqa8j56tg9ryl.r003 -t 1 --cpu-affinity $i &
-            done
-done
